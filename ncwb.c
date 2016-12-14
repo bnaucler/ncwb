a6e82ea6aa711fbbcb171a9ac452d73b81591dce
bnaucler <mail@bnaucler.se>
2016-12-14 13:33:08 +0100

    Introducing getopt

--- ncwb.c
+++ ncwb.c
@@ -5,6 +5,21 @@
 
 #define SMAXLEN 50
 
+int ncinit() {
+	
+	initscr();
+	noecho();
+	cbreak();
+	curs_set(0);
+	start_color();
+	use_default_colors();
+
+	init_pair(1, COLOR_RED, -1);
+	init_pair(2, COLOR_BLACK, COLOR_RED);
+
+	return 0;
+}
+
 WINDOW *mkwin(char *str, int wh, int ww, int wsy, int wsx, int pad, int cp) {	
 
 	WINDOW *lwin;
@@ -28,30 +43,56 @@ void dwin(WINDOW *lwin) {
 	delwin(lwin);
 }
 
+int usage(char *cmd) {
+
+	printf("Usage: %s [-dsp]]\n", cmd);
+	printf("	-d: Show debug information\n");
+	printf("	-s [ms]: Sleep per cycle\n");
+	printf("	-p [size]: Char size in padding\n");
+	return 1;
+}
+
 int main(int argc, char *argv[]) {
 
-	WINDOW *txwin;
-
-	initscr();
-	noecho();
-	cbreak();
-	curs_set(0);
-	start_color();
-	use_default_colors();
-
-	init_pair(1, COLOR_RED, -1);
-	init_pair(2, COLOR_BLACK, COLOR_RED);
-
 	char str[SMAXLEN];
+	int debug = 0;
+	int pad = 2;
 
-	if (argc == 1) {
-		strcpy(str, getenv("USER")); 
-	} else {
-		if (strlen(argv[1]) < SMAXLEN) strcpy(str, argv[1]); 
-		else return 1;
+	long slp = 100000;
+
+	int optc;
+
+	while((optc = getopt(argc, argv, "ds:p:")) != -1) {
+		switch (optc) {
+
+			case 'd':
+				debug++;
+				break;
+
+			case 's':
+				slp = atoi(optarg) * 1000;
+				if (slp == 0) return usage(argv[0]);
+				break;
+
+			case 'p':
+				pad = atoi(optarg);
+				break;
+
+			case '?':
+				return usage(argv[0]);
+				break;
+
+			default:
+				break;
+		}
 	}
 
-	int pad = 2;
+	if (argc > optind) strcpy(str, argv[optind]); 
+	else strcpy(str, getenv("USER"));
+
+	ncinit();
+	WINDOW *txwin;
+
 	int cp = 2;
 	int yctr = LINES / 2;
 	int xctr = COLS / 2;
@@ -62,19 +103,20 @@ int main(int argc, char *argv[]) {
 	int wsy = yctr - (pad + 1);
 	int wsx = xctr - ((len / 2) + pad);
 
-	int xi = 0;
-	int yi = 0;
+	int xi = 0, yi = 0;
 
 	bkgd(COLOR_PAIR(1));
 	refresh();
 
-	mvprintw(5,5,"wh: %d", wh);
-	mvprintw(6,5,"ww: %d", ww);
-	mvprintw(7,5,"wsy: %d", wsy);
-	mvprintw(8,5,"wsx: %d", wsx);
-	mvprintw(9,5,"len: %d", len);
-	mvprintw(10,5,"yctr: %d", yctr);
-	mvprintw(11,5,"xctr: %d", xctr);
+	if (debug) {
+		mvprintw(5,5,"wh: %d", wh);
+		mvprintw(6,5,"ww: %d", ww);
+		mvprintw(7,5,"wsy: %d", wsy);
+		mvprintw(8,5,"wsx: %d", wsx);
+		mvprintw(9,5,"len: %d", len);
+		mvprintw(10,5,"yctr: %d", yctr);
+		mvprintw(11,5,"xctr: %d", xctr);
+	}
 
 	txwin = mkwin(str, wh, ww, wsy, wsx, pad, cp);
 
@@ -98,15 +140,17 @@ int main(int argc, char *argv[]) {
 			if (wsy > LINES - wh) yi = 0;
 		}
 
-		mvprintw(7,5,"wsy: %d", wsy);
-		mvprintw(8,5,"wsx: %d", wsx);
+		if (debug) {
+			mvprintw(7,5,"wsy: %d", wsy);
+			mvprintw(8,5,"wsx: %d", wsx);
+		}
 
 		mvwin(txwin, wsy, wsx);
 		wbkgd(txwin, COLOR_PAIR(cp));
 		bkgd(COLOR_PAIR(1));
 		refresh();
 		wrefresh(txwin);
-		usleep(100000);
+		usleep(slp);
 	}
 
 	dwin(txwin);
